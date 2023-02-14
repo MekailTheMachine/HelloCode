2cab12acb94cd83af24af16e9bc8ad81bd1d546b
Mekail The Machine <mekailmurphy@gmail.com>
2023-02-13 23:32:26 -0700

    Added HelloJS and HelloFrontEnd

---
+++ HelloJavaScript.js
@@ -0,0 +1,11 @@
+/* JS is a high-level and interpreted language, typically used to add interactivity and dynamic behaviour to websites. 
+
+JavaScript is also a dynamically typed language, allowing for more flexibility, as variables can be assigned values of different types without 
+requiring explicit type declarations.
+However, dynamic typing can also lead to errors and make it more difficult to reason about code.
+To mitigate this, JavaScript has added features such as TypeScript, which is a statically typed superset of JavaScript
+
+Below is Hello Js, in JavaScript */
+
+
+console.log("Hello, JS!");
